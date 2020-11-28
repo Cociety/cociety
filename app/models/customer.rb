b43a79854b285eb38b942ec48b271682727aab14
Justin Robinson <justinorobinson@icloud.com>
2020-11-28 00:45:35 -0800

    create externable concern and add to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
   before_save { last_name&.strip! }
   has_secure_password
   has_many :emails, class_name: "CustomerEmail", autosave: true
+  has_many :external_entities, as: :internal_entity
   has_many :payment_allocation_sets, autosave: true
   validates :first_name, presence: true, allow_blank: false
   validates :last_name, presence: true, allow_blank: false
