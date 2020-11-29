fee29888f5ede5d20134d0221c4a6122e2e40361
Justin Robinson <justinorobinson@icloud.com>
2020-11-29 14:01:44 -0800

    replace 4 indents with 2

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ApplicationRecord
-    belongs_to :owner, polymorphic: true, inverse_of: :account
-    has_many :payments
-    scope :customer, -> { where(owner_type: Customer.name) }
-    scope :organization, -> { where(owner_type: Organization.name) }
+  belongs_to :owner, polymorphic: true, inverse_of: :account
+  has_many :payments
+  scope :customer, -> { where(owner_type: Customer.name) }
+  scope :organization, -> { where(owner_type: Organization.name) }
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,37 +1,37 @@
 class Payment < ApplicationRecord
-    after_create :create_stripe_charge
-    belongs_to :account
-    has_one :external_entity, as: :internal_entity
-    monetize :amount_cents
-    scope :from_customers, -> (range) { where(created_at: range).joins(:account).merge(Account.customer) }
-    scope :from_organizations, -> (range) { where(created_at: range).joins(:account).merge(Account.organization) }
+  after_create :create_stripe_charge
+  belongs_to :account
+  has_one :external_entity, as: :internal_entity
+  monetize :amount_cents
+  scope :from_customers, -> (range) { where(created_at: range).joins(:account).merge(Account.customer) }
+  scope :from_organizations, -> (range) { where(created_at: range).joins(:account).merge(Account.organization) }
 
-    def create_stripe_charge
-        CreateStripeCharge.perform_async(id)
+  def create_stripe_charge
+    CreateStripeCharge.perform_async(id)
+  end
+
+  # Gets all customer's payments from a specific time window (range) and calculates how much to pay out to each organization
+  def self.calculate_organization_payouts(range)
+    customer_payments_in_range = self.from_customers(range).includes(account: { owner: {payment_allocation_sets: { payment_allocations: { organization: :account}}}} )
+
+    ungrouped_organization_payouts = customer_payments_in_range.flat_map do |p|
+      customer_payment_allocations = p.account.owner.payment_allocations
+
+      customer_payment_allocations.map do |a|
+        amount = p.amount * a.percent / 100
+        account = a.organization.account
+        Payment.new(
+          amount: amount,
+          account: account
+        )
+      end
     end
 
-    # Gets all customer's payments from a specific time window (range) and calculates how much to pay out to each organization
-    def self.calculate_organization_payouts(range)
-        customer_payments_in_range = self.from_customers(range).includes(account: { owner: {payment_allocation_sets: { payment_allocations: { organization: :account}}}} )
-
-        ungrouped_organization_payouts = customer_payments_in_range.flat_map do |p|
-            customer_payment_allocations = p.account.owner.payment_allocations
-
-            customer_payment_allocations.map do |a|
-                amount = p.amount * a.percent / 100
-                account = a.organization.account
-                Payment.new(
-                    amount: amount,
-                    account: account
-                )
-            end
-        end
-
-        ungrouped_organization_payouts.group_by(&:account).map do |organziation, payments|
-            payments.reduce do |sum, p|
-                sum.amount += p.amount
-                sum
-            end
-        end
+    ungrouped_organization_payouts.group_by(&:account).map do |organziation, payments|
+      payments.reduce do |sum, p|
+        sum.amount += p.amount
+        sum
+      end
     end
+  end
 end
