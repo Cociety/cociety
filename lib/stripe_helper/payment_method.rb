2b9526db769e769e4619940cb9142d40c53808cf
Justin Robinson <justinorobinson@icloud.com>
2020-11-29 18:41:57 -0800

    api to create and get a customer's payment methods

---
+++ lib/stripe_helper/payment_method.rb
@@ -0,0 +1,31 @@
+module StripeHelper
+  class PaymentMethod
+
+    #
+    # StripeHelper::PaymentMethod.create_card("cus_123", {
+    #      number: "4111111111111111",
+    #      exp_month: 11,
+    #      exp_year: 2021,
+    #      cvc: "314"
+    # })
+    # https://stripe.com/docs/api/tokens/create_card
+    # https://stripe.com/docs/api/cards/create
+    #
+    def self.create_card(stripe_id, card_args)
+      token = Stripe::Token.create({
+        card: card_args,
+      }).id
+      Stripe::Customer.create_source(
+        stripe_id,
+        { source: token }
+      )
+    end
+
+    def self.get_cards(stripe_id)
+      Stripe::PaymentMethod.list({
+        customer: stripe_id,
+        type: "card"
+      })["data"]
+    end
+  end
+end
