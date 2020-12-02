04efe62d0341cbc33ac944cc98ff75f4b41083c0
Justin Robinson <justinorobinson@icloud.com>
2020-12-01 16:14:20 -0800

    rubocop fixes for SecureUrlValidator

--- app/models/concerns/secure_url_validator.rb
+++ app/models/concerns/secure_url_validator.rb
@@ -1,11 +1,16 @@
+# Rails Validator to test if a value is a secure url
+# Example
+# class HasSecureUrlProperty < ActiveRecord::Base
+#   validates :url_property, presence: true, secure_url: true
+# end
 class SecureUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, 'is not a valid HTTPS URL') unless is_secure_url?(value)
+    record.errors.add(attribute, 'is not a valid HTTPS URL') unless secure_url?(value)
   end
 
   private
 
-  def is_secure_url?(url)
+  def secure_url?(url)
     uri = URI.parse(url)
     uri.is_a?(URI::HTTPS) && uri.host.present?
   rescue StandardError
