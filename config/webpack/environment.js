a23eb451457b09e15d846df6c4a910ef04b27715
Justin Robinson <justinorobinson@icloud.com>
2020-12-12 15:23:14 -0800

    upgrade to tailwind 2.0.2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+const css = environment.loaders.get('css')
+const sass = environment.loaders.get('sass')
+
+const postCssConfig = css.use.find(u => u.loader === 'postcss-loader')
+const postSassConfig = sass.use.find(u => u.loader === 'postcss-loader')
+
+if (postCssConfig) {
+  delete postCssConfig.options.config
+}
+
+if (postSassConfig) {
+  delete postCssConfig.options.config
+}
+
 module.exports = environment
