3620955775c38ceefea3f941ad673adf691e9dc7
Justin Robinson <justinorobinson@icloud.com>
2020-12-15 03:03:41 -0800

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require("tailwindcss"),
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
