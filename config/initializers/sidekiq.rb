b88f78e9028de8f9ff432afed48da5236fa04e6f
Justin Robinson <justinorobinson@icloud.com>
2021-01-03 18:20:06 -0800

    rename sidekiq namespace to cociety_jobs from oss_sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'], namespace: "oss_sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'], namespace: "cociety_jobs_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'], namespace: "oss_sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'], namespace: "cociety_jobs_#{Rails.env}" }
 end
 
 ENV['SIDEKIQ_REDIS_URL'] ||= "#{ENV['REDIS_URL']}/0"
