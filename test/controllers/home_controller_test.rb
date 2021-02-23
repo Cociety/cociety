2c8cdfb7a153058c7fc1fe3a7cd4316a7fbe0ffd
Justin Robinson <justinorobinson@icloud.com>
2021-02-23 02:10:54 -0800

    Revert "temporarily disable sign ups"
    This reverts commit 5ce04f1ca4deff7db372ccd93d402bacbbf416f7.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,18 +11,20 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'title', 'Cociety [test]'
   end
 
-  test 'should show the nav bar with login button when logged out' do
+  test 'should show the nav bar with signup and login buttons when logged out' do
     sign_out :customer
     get root_path
     assert_select 'nav' do
-      assert_select 'a.btn', 2 # 1 for mobile and 1 for desktop
+      assert_select 'a.btn', 4 # two for mobile and 2 for desktop
+      assert_select 'a.btn.btn-primary', { count: 2, text: 'Sign up' }
       assert_select 'a.btn.btn-secondary', { count: 2, text: 'Login' }
     end
   end
 
-  test 'should not show login button when logged in' do
+  test 'should not show login and signup buttons when logged in' do
     get root_path
     assert_select 'nav' do
+      assert_select 'a.btn.btn-primary', { count: 0, text: 'Sign up' }
       assert_select 'a.btn.btn-secondary', { count: 0, text: 'Login' }
     end
   end
